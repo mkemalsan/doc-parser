b80cbe54f99b45d5570d644aa076bd7902c97de0
Kemal <mkemalsan@gmail.com>
2021-02-26 18:39:09 +0100

    Fixed JSON req.body

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+app.use(express.json()) // for parsing application/json
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    res.send("test")
+    res.send(req.body)
 
 })
 
