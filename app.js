530be878889bb6acf5223070e101f468681a0ef9
Kemal <mkemalsan@gmail.com>
2021-02-26 21:53:51 +0100

    Larger data size limit

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({
+  extended: true, limit: '50mb'}));
 
-
-app.use(express.json()) // for parsing application/json
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+
+
+
+function parseDoc(){
+
+}
+
+
+
+
 app.post('/', (req, res) => {
-    console.log(req.body)
+
+
+
+
+
+
     res.send(req.body)
 
 })
